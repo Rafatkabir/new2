782bd856fe5e456b4ca85c13d5c4256136a76d2b
Code-Knight02 <124998258+Code-Knight02@users.noreply.github.com>
2024-03-10 05:42:24 +0600

    first commit

---
+++ E/in/generatecase.cpp
@@ -0,0 +1,179 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct info{
+    int weight;
+    int parent;
+};
+struct item{
+    int value;
+    int weight;
+};
+
+
+vector<vector<int>>dp;
+vector<item>elements, temp;
+double theta;
+set<pair<int, int>>s;
+int n, w; 
+int opt(int i, int v)
+{
+    
+    if(v <= 0) return 0;
+    if(i == 0) return 1e9;
+    if(dp[i][v] != 1e9) return dp[i][v];
+    int without =  opt(i-1, v);
+    int with = opt(i-1, v-elements[i].value) + elements[i].weight;
+    return dp[i][v] = min(with, without);
+}
+int max_elem, tot_value, original_answer;
+
+string format_float(double x)
+{
+    string s = to_string(x);
+    if(s.find('.') != string::npos){
+        while(!s.empty() && s.back() == '0') 
+            s.pop_back();        
+        if(s.back() == '.') s += '0';
+    }
+    return s;
+}
+
+
+bool report(double epsilon)
+{
+    
+    theta = epsilon * max_elem / 2 / n;
+    // cout << "Rounded Instance with Eps: " << fixed << format_float(epsilon) << "\n";
+    // cout << "Theta: " << fixed << setprecision(16) <<  theta << "\n";
+    tot_value = 0;
+    for(int i = 1; i <= n; i++) {
+        double x = elements[i].value/theta;
+        elements[i].value = (int)(x);
+        elements[i].value++;
+        tot_value += elements[i].value;
+    }
+    // for(auto x : elements) cout << "(" << x.value << ", " << x.weight << ")\n";
+    for(auto &x : dp) x.clear();
+    dp.clear();
+    dp.resize(n + 1);
+    for(int i = 0; i <= n; i++) dp[i].resize(tot_value + 1, 1e9);    
+    for(int i = 0; i <= n; i++) dp[i][0] = 0;
+    // for(int i = 0; i <= n; i++){
+    //     for(int j = 0; j <= tot_value; j++){
+    //         cout << dp[i][j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
+    for(int i = 1; i <= n; i++){
+        for(int v = 1; v <= tot_value; v++){
+            dp[i][v] = dp[i-1][v];
+            if(v-elements[i].value >= 0){
+                dp[i][v] = min(dp[i][v], dp[i-1][v-elements[i].value] + elements[i].weight);
+            }
+        }
+    }
+    int ans = 0, idx = 0;
+    for(int v = tot_value; v >= 0; v--){
+        if(dp[n][v] <= w){
+            ans = v;
+            idx = v;            
+            break;
+        }
+    }
+    // cout << "Answer of reduced instance: " << ans << "\n";
+    double achieved_answer = ans * theta;
+    // cout << "Answer of reduced instance multiplied by theta: " 
+    //      << fixed << setprecision(12) << achieved_answer << "\n";
+    // cout << "Indices: ";
+    int rounded_up_answer = 0, weight = 0;
+    for(int i = n; i >= 1; i--){
+        int x = dp[i-1][idx];
+        int y = 1e9;
+        if(idx - elements[i].value >= 0)  y = dp[i-1][idx - elements[i].value];
+        if(y + elements[i].weight == dp[i][idx]){
+            // cout << i << " ";
+            idx -= elements[i].value;
+            rounded_up_answer += temp[i].value;
+            weight += temp[i].weight;
+        }
+    }
+    // cout << "\nAnswer of original instance (rounded up): " << rounded_up_answer << "\n";
+    // cout << "Used Weight: " << weight << "\n";
+    double ratio = 1.0 * original_answer/rounded_up_answer;
+    // cout << "Ratio: ";
+    // if(ratio == 1.0) 
+    //     cout << "1.0\n\n";
+    // else
+    //     cout << fixed << setprecision(16) << ratio << "\n\n";
+    cout << format_float(epsilon) << "->" << ratio << ", ";
+    return ratio > (1 + 0.7*epsilon);
+}
+
+void original_instance()
+{
+    dp.resize(n + 1);
+    for(int i = 0; i <= n; i++) dp[i].resize(tot_value + 1, 1e9);
+    for(int i = 0; i <= n; i++) dp[i][0] = 0;
+    for(int i = 1; i <= n; i++){
+        for(int v = 1; v <= tot_value; v++){
+            dp[i][v] = dp[i-1][v];
+            if(v-elements[i].value >= 0){
+                dp[i][v] = min(dp[i][v], dp[i-1][v-elements[i].value] + elements[i].weight);
+            }
+        }
+    }
+    int ans = 0, idx = 0;
+    for(int v = tot_value; v >= 0; v--){
+        if(dp[n][v] <= w){
+            ans = v;
+            idx = v;            
+            break;
+        }
+    }
+    // cout << "Original Instance: \nAnswer: " << ans << "\n";
+    int weight = 0;
+    vector<int>indices;
+    for(int i = n; i >= 1; i--){
+        int x = dp[i-1][idx];
+        int y = 1e9;
+        if(idx - elements[i].value >= 0)  y = dp[i-1][idx - elements[i].value];
+        if(y + elements[i].weight == dp[i][idx]){            
+            indices.push_back(i);
+            idx -= elements[i].value;
+            weight += elements[i].weight;
+        }
+    }
+    // cout << "Used Weight: " << weight << "\n";
+    // cout << "Indices: ";
+    // for(auto x : indices) cout << x << " ";
+    original_answer = ans;
+    // cout << "\n\n";
+}
+
+int main()
+{   
+    freopen("t2.txt", "w", stdout);
+    srand(time(NULL));
+    // int n12700 = rand() % 50;
+    // int n12595 = rand() % 50;
+    // int n1 = rand() % 50;
+    // int w = rand() % 100;
+    // cout << n12595 + n12700 + n1 << "\n" <<  w << "\n";
+    // for(int i = 0; i < n12595 + n12700 + n1; i++){
+
+    // }
+    int n ; cin >> n >> w;
+    cout << n << "\n" << w << "\n";
+    cout << "12700 3\n12595 2\n1 1\n";
+    for(int i = 0; i < n - 3; i++){
+        cout << "1 " << w + 1 << "\n";
+    }
+
+
+
+
+
+
+    return 0;
+}
---
+++ generatecase.cpp
@@ -0,0 +1,195 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct info{
+    int weight;
+    int parent;
+};
+struct item{
+    int value;
+    int weight;
+};
+
+
+vector<vector<int>>dp;
+vector<item>elements, temp;
+double theta;
+set<pair<int, int>>s;
+int n, w; 
+int opt(int i, int v)
+{
+    
+    if(v <= 0) return 0;
+    if(i == 0) return 1e9;
+    if(dp[i][v] != 1e9) return dp[i][v];
+    int without =  opt(i-1, v);
+    int with = opt(i-1, v-elements[i].value) + elements[i].weight;
+    return dp[i][v] = min(with, without);
+}
+int max_elem, tot_value, original_answer;
+
+string format_float(double x)
+{
+    string s = to_string(x);
+    if(s.find('.') != string::npos){
+        while(!s.empty() && s.back() == '0') 
+            s.pop_back();        
+        if(s.back() == '.') s += '0';
+    }
+    return s;
+}
+
+
+bool report(double epsilon)
+{
+    
+    theta = epsilon * max_elem / 2 / n;
+    // cout << "Rounded Instance with Eps: " << fixed << format_float(epsilon) << "\n";
+    // cout << "Theta: " << fixed << setprecision(16) <<  theta << "\n";
+    tot_value = 0;
+    for(int i = 1; i <= n; i++) {
+        double x = elements[i].value/theta;
+        elements[i].value = (int)(x);
+        elements[i].value++;
+        tot_value += elements[i].value;
+    }
+    // for(auto x : elements) cout << "(" << x.value << ", " << x.weight << ")\n";
+    for(auto &x : dp) x.clear();
+    dp.clear();
+    dp.resize(n + 1);
+    for(int i = 0; i <= n; i++) dp[i].resize(tot_value + 1, 1e9);    
+    for(int i = 0; i <= n; i++) dp[i][0] = 0;
+    // for(int i = 0; i <= n; i++){
+    //     for(int j = 0; j <= tot_value; j++){
+    //         cout << dp[i][j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
+    for(int i = 1; i <= n; i++){
+        for(int v = 1; v <= tot_value; v++){
+            dp[i][v] = dp[i-1][v];
+            if(v-elements[i].value >= 0){
+                dp[i][v] = min(dp[i][v], dp[i-1][v-elements[i].value] + elements[i].weight);
+            }
+        }
+    }
+    int ans = 0, idx = 0;
+    for(int v = tot_value; v >= 0; v--){
+        if(dp[n][v] <= w){
+            ans = v;
+            idx = v;            
+            break;
+        }
+    }
+    // cout << "Answer of reduced instance: " << ans << "\n";
+    double achieved_answer = ans * theta;
+    // cout << "Answer of reduced instance multiplied by theta: " 
+    //      << fixed << setprecision(12) << achieved_answer << "\n";
+    // cout << "Indices: ";
+    int rounded_up_answer = 0, weight = 0;
+    for(int i = n; i >= 1; i--){
+        int x = dp[i-1][idx];
+        int y = 1e9;
+        if(idx - elements[i].value >= 0)  y = dp[i-1][idx - elements[i].value];
+        if(y + elements[i].weight == dp[i][idx]){
+            // cout << i << " ";
+            idx -= elements[i].value;
+            rounded_up_answer += temp[i].value;
+            weight += temp[i].weight;
+        }
+    }
+    // cout << "\nAnswer of original instance (rounded up): " << rounded_up_answer << "\n";
+    // cout << "Used Weight: " << weight << "\n";
+    double ratio = 1.0 * original_answer/rounded_up_answer;
+    // cout << "Ratio: ";
+    // if(ratio == 1.0) 
+    //     cout << "1.0\n\n";
+    // else
+    //     cout << fixed << setprecision(16) << ratio << "\n\n";
+    cout << format_float(epsilon) << "->" << ratio << ", ";
+    return ratio > (1 + 0.7*epsilon);
+}
+
+void original_instance()
+{
+    dp.resize(n + 1);
+    for(int i = 0; i <= n; i++) dp[i].resize(tot_value + 1, 1e9);
+    for(int i = 0; i <= n; i++) dp[i][0] = 0;
+    for(int i = 1; i <= n; i++){
+        for(int v = 1; v <= tot_value; v++){
+            dp[i][v] = dp[i-1][v];
+            if(v-elements[i].value >= 0){
+                dp[i][v] = min(dp[i][v], dp[i-1][v-elements[i].value] + elements[i].weight);
+            }
+        }
+    }
+    int ans = 0, idx = 0;
+    for(int v = tot_value; v >= 0; v--){
+        if(dp[n][v] <= w){
+            ans = v;
+            idx = v;            
+            break;
+        }
+    }
+    // cout << "Original Instance: \nAnswer: " << ans << "\n";
+    int weight = 0;
+    vector<int>indices;
+    for(int i = n; i >= 1; i--){
+        int x = dp[i-1][idx];
+        int y = 1e9;
+        if(idx - elements[i].value >= 0)  y = dp[i-1][idx - elements[i].value];
+        if(y + elements[i].weight == dp[i][idx]){            
+            indices.push_back(i);
+            idx -= elements[i].value;
+            weight += elements[i].weight;
+        }
+    }
+    // cout << "Used Weight: " << weight << "\n";
+    // cout << "Indices: ";
+    // for(auto x : indices) cout << x << " ";
+    original_answer = ans;
+    // cout << "\n\n";
+}
+
+int main()
+{
+    ofstream fout("testcase.txt");
+    srand(time(NULL));
+    while(true){
+        info dummy = {INT_MAX, -1};
+        // cin >> n>> w;
+        n = rand() % 5 + 3;
+        w = rand() % 4 + 4;
+        int v = rand()%1000000 + 1000000;
+        cout << "n = " << n << " " << " w = " <<  w  << " v = " << v << "\n";
+        elements.resize(n + 1);
+        tot_value = max_elem = 0;
+        for(int i = 1; i <= n; i++) {
+            // cin >> elements[i].value >> elements[i].weight;
+            elements[i].value = rand() % v + 1;
+            elements[i].weight = rand() % w + 1;
+            tot_value += elements[i].value;
+            max_elem = max(max_elem, elements[i].value);
+        }
+        original_instance();
+        temp = elements;
+        bool a1 = report(0.5);
+        elements = temp;
+        bool a2 = report(0.2);
+        elements = temp;
+        bool a3 = report(0.1);
+        elements = temp;
+        bool a4 = report(0.05);
+        if(a1 && a2 && a3 && a4){
+            fout << "n = " << n << " w = " << w << "\n";
+            for(auto x : elements) fout << x.value << " " << x.weight << "\n";
+            cout << "n = " << n << " w = " << w << "\n";
+            for(auto x : elements) cout << x.value << " " << x.weight << "\n";
+            break;
+        }
+        else{
+            cout << "no luck\n";
+        }
+    }
+    return 0;
+}
